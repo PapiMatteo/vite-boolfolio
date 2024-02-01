5bb015ad32da3019f54e958cbf7e2193de47499a
PapiMatteo <matteopapi.work@gmail.com>
2024-02-01 14:02:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+ <h1>Hello World
+ </h1>
+</template>
+
+<style lang="scss">
+@use './style/general.scss';
+</style>
